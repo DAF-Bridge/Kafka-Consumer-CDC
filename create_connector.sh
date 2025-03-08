6155c5139b2f75884b103ae5de4e84d3dc5b4ce8
andaraiwin <andaraiwin@gmail.com>
2025-03-09 03:58:02 +0700

    update shell script

--- create_connector.sh
+++ create_connector.sh
@@ -14,27 +14,27 @@ echo "Waiting for Debezium to be available..."
 while ! nc -z debezium 8083; do sleep 1; done
 
 # Create the Debezium connector
-# echo "Creating the Debezium PostgreSQL connector..."
-# curl -X POST http://debezium:8083/connectors \
-#   -H "Content-Type: application/json" \
-#   -d '{
-#     "name": "postgres-cdc-connector",
-#     "config": {
-#       "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-#       "database.hostname": "postgres",
-#       "database.port": "5432",
-#       "database.user": "postgres",
-#       "database.password": "postgres",
-#       "database.dbname": "talents_atmos",
-#       "database.server.name": "cdc",
-#       "slot.name": "debezium_slot",
-#       "plugin.name": "pgoutput",
-#       "table.include.list": "public.events, public.org_open_jobs, public.organization",
-#       "topic.prefix": "cdc",
-#       "database.history.kafka.bootstrap.servers": "kafka:9092",
-#       "database.history.kafka.topic": "schema-changes.talents_atmos"
-#     }
-#   }'
+echo "Creating the Debezium PostgreSQL connector..."
+curl -X POST http://localhost:8083/connectors \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "postgres-cdc-connector",
+    "config": {
+      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+      "database.hostname": "postgres",
+      "database.port": "5432",
+      "database.user": "postgres",
+      "database.password": "postgres",
+      "database.dbname": "talents_atmos",
+      "database.server.name": "cdc",
+      "slot.name": "debezium_slot",
+      "plugin.name": "pgoutput",
+      "table.include.list": "public.events, public.org_open_jobs, public.organizations",
+      "topic.prefix": "cdc",
+      "database.history.kafka.bootstrap.servers": "localhost:9092",
+      "database.history.kafka.topic": "schema-changes.talents_atmos"
+    }
+  }'
 
 # Wait for the connector to be fully created
 echo "Waiting for the connector to be fully created..."
@@ -42,7 +42,7 @@ sleep 5
 
 # Check the connector status
 echo "Checking the connector status..."
-connector_status=$(curl -s http://debezium:8083/connectors/postgres-cdc-connector/status)
+connector_status=$(curl -s http://localhost:8083/connectors/postgres-cdc-connector/status)
 echo "Connector Status: $connector_status"
 
 # Check if the connector is running
@@ -55,13 +55,13 @@ fi
 
 # Check the connector group
 # echo "Checking the connector group..."
-# connector_group=$(curl -s http://debezium:8083/connectors/postgres-cdc-connector/tasks)
+# connector_group=$(curl -s http://localhost:8083/connectors/postgres-cdc-connector/tasks)
 # echo "Connector Group: $connector_group"
 
 # # Check if the Kafka topic is created
 # topic_name="cdc.public.events"
 # echo "Checking if Kafka topic $topic_name exists..."
-# topic_exists=$(kafka-topics --bootstrap-server kafka:9092 --list | grep -w "$topic_name")
+# topic_exists=$(kafka-topics --bootstrap-server localhost:9092 --list | grep -w "$topic_name")
 # if [ "$topic_exists" == "$topic_name" ]; then
 #     echo "Topic $topic_name exists."
 # else
